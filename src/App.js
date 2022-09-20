cd15cd79101137881d8fb1d79358f9a55d8ad845
Mirza N Ahmed <ahmed.mir@husky.neu.edu>
2022-09-20 04:02:29 -0400

    assignment 2 start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import WelcomeToFSD from "./WelcomeToFSD";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <WelcomeToFSD/>
+        <h3>Assignment 2</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/WelcomeToFSD.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const WelcomeToFSD = () => {
+    return (
+        <h1>Welcomw to Full Stack Dev</h1>
+    )
+}
+
+export default WelcomeToFSD;
